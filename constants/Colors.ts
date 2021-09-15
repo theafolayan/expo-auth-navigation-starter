324cd85360494081af965a90a41b621026715ed2
theafolayan <theafolayan@gmail.com>
2021-09-15 17:34:44 +0100

    initial commit

---
+++ constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = '#';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#242333',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+};
